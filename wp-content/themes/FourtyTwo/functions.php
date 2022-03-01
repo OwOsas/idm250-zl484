64082641b8aca727060703b4edeb5535561057f9
Sylar Li <65619327+OwOsas@users.noreply.github.com>
2022-02-28 22:32:18 -0500

    Custom Post type

--- wp-content/themes/FourtyTwo/functions.php
+++ wp-content/themes/FourtyTwo/functions.php
@@ -61,6 +61,45 @@ add_action('after_setup_theme', 'register_theme_navigation');
 
 // Custom Post Type
 function fortyTwo_register_custom_post_type(){
-    $args = [];
-    register_post_type( 'fortyTwo_portfolio', $args);
+
+    $args = [
+        'label' => 'Project',
+        'labels' => [
+            'name' => 'Projects',
+            'singular_name' => 'Project'
+        ],
+        'supports' => [
+            'title',
+            'editor',
+            'author',
+            'thumbnail',
+            'excerpt',
+            'trackbacks',
+            'custom-fields',
+            'comments',
+            'revisions',
+            'page-attributes',
+            'post-formats'
+        ],
+        // 'taxonomies'            => ['category', 'post_tag'],
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'show_in_rest' => true,
+        // Dash Icons https://developer.wordpress.org/resource/dashicons/#media-audio
+        'menu_icon' => 'dashicons-clipboard'
+        // 'menu_icon'             => get_stylesheet_directory_uri() . '/static/images/icons/industries.png'
+    ];
+
+    register_post_type( 'idm_portfolio', $args);
 }
+
+add_action('init', 'fortyTwo_register_custom_post_type');
